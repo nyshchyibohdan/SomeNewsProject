6c15be0079ef2ced3d122f0c8d16f38d30b4d76b
nyshchyibohdan <nyshchyi.bohdan@student.uzhnu.edu.ua>
2024-10-02 12:12:23 +0300

    configured eslint and added cypress.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('node:path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
@@ -30,7 +30,6 @@ const config = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
-    mode: "development"
 };
 
 module.exports = config;
