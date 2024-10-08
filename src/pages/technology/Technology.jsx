91d89774972a286fea1456ace010fe15042dfaf8
nyshchyibohdan <nyshchyi.bohdan@student.uzhnu.edu.ua>
2024-10-08 19:17:00 +0300

    fixed imports

--- src/pages/technology/Technology.jsx
+++ src/pages/technology/Technology.jsx
@@ -2,7 +2,7 @@ import '../../components/newsPages/NewsPages.css';
 
 import React from 'react';
 
-import NewsPages from '../../components/newsPages/NewsPages';
+import { NewsPages } from '../../components/index';
 
 function Technology() {
     const apiRoute = 'http://localhost:5000/api/newsapi/tech';
